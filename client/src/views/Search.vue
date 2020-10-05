a04894ae6a826e8ffb3b674a80c0413015d62081
omputle <omputle@student.wethinkcode.co.za>
2020-10-05 09:35:04 +0200

    testing new search field

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -1,17 +1,32 @@
 <template>
     <div>
+        <h1>Search Songs</h1>
         <p>{{artist}}</p>
         <input type="search" name="" id="" v-model="artist">
         <button @click="search">Search</button>
+        <div>
+            <input type="search" name="" id="" v-model="album" placeholder="Search Album">
+            <button @click="search_album">Search Album</button>
+        </div>
 
         <div v-for="file in info" :key="file">
             <div>
+                <router-link :to="'/album/' + file.album.id">
                 <img :src="file.album.cover" alt="">
                 <p>{{file.title}}</p>
                 <p>{{file.artist.name}}</p>
                 <small>Album: {{file.album.title}}</small>
+                </router-link>
             </div>
             <hr>
+        </div>
+
+        <div v-for="info in albums_found" :key="info">
+            <router-link :to="'/album/' + info.album.id">
+                <img :src="info.album.cover_medium" alt="">
+                <p>{{info.title}}</p>
+                <small>{{info.artist.name}}</small>
+            </router-link>
         </div>
     </div>
 </template>
@@ -23,6 +38,8 @@ export default {
     data() {
         return {
             artist: '',
+            album: '',
+            albums_found: {},
             info: []
         }
     },
@@ -37,6 +54,15 @@ export default {
             axios.get(url).then((res) => {
                 console.log(res.data.data)
                 this.info = res.data.data
+            }).catch((err) => {
+                console.log(err)
+            })
+        },
+        search_album() {
+            let url = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=album:"'+ this.album +'"'
+            axios.get(url).then((res) => {
+                console.log(res)
+                this.albums_found = res.data.data
             }).catch((err) => {
                 console.log(err)
             })
