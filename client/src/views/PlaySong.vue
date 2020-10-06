b49276b8ea4946a274a920922a8066843684a4be
omputle <omputle@student.wethinkcode.co.za>
2020-10-06 14:23:18 +0200

    added new views

---
+++ client/src/views/PlaySong.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <router-link v-if="artist_id" :to="'/artist/' + artist_id" >
+            <h1>{{artist_name}}</h1>
+        </router-link>
+        <img :src="cover" alt="">
+        <h3>{{song_name}}</h3>
+        <router-link :to="'/album/' + album_id" v-if="album_id">
+            <p>{{album_name}}</p>
+        </router-link>
+        <audio :src="preview" controls></audio>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            song_id: this.$route.params.id,
+            cover: '',
+            artist_name: '',
+            artist_id: '',
+            song_name: '',
+            album_name: '',
+            preview: '',
+            album_id: ''
+        }
+    },
+    methods: {
+        fetch_data() {
+            let url = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/';
+            axios.get(url + this.song_id).then((results) => {
+                console.log(results)
+                this.artist_name = results.data.artist.name
+                this.artist_id = results.data.artist.id
+                this.cover = results.data.artist.picture_big
+                this.song_name = results.data.title
+                this.album_name = results.data.album.title
+                this.preview = results.data.preview
+                this.album_id = results.data.album.id
+            }).catch((err) => {
+                console.log(err)
+            })
+        },
+    },
+    created() {
+        this.fetch_data()
+    }
+}
+</script>
