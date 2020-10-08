275f94df8afb59f43758fa98d0f8cd3ee880397f
omputle <omputle@student.wethinkcode.co.za>
2020-10-08 12:00:39 +0200

    added playlist page

---
+++ client/src/views/Playlist.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <h2>{{playlist_name}}</h2>
+        <img :src="cover" alt=""> <br>
+        <p>{{creator}}</p>
+        <div class="songs">
+            <ol>
+                <div v-for="song in songs" :key="song">
+                    <li @click="play_track(song)">{{song.title}} - {{song.artist.name}}</li>
+                </div>
+            </ol>
+        </div>
+        <div>
+            <audio :src="play" controls v-if="play" autoplay></audio>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            playlist_id: this.$route.params.id,
+            songs: [],
+            creator: '',
+            cover: '',
+            playlist_name: '',
+            play: ''
+        }
+    },
+    methods: {
+        fetch_data() {
+            let url = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/'
+            axios.get(url + this.playlist_id).then((results) => {
+                console.log(results)
+                this.playlist_name = results.data.title
+                this.creator = results.data.creator.name
+                this.cover = results.data.picture_big
+                this.songs = results.data.tracks.data
+            }).catch((err) => {
+                console.log(err)
+            })
+        },
+        play_track(song) {
+            this.play = song.preview
+        }
+    },
+    created() {
+        this.fetch_data()
+    }
+}
+</script>
+
+<style scoped>
+li {
+    text-align: left;
+}
+li:hover {
+    color: brown;
+}
+.songs {
+    float: right;
+}
+</style>
---
+++ client/src/views/okay.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <button @click="fetch_list">Fetch Playlists</button> <br>
+        {{playlists[0]}}
+        <div v-for="playlist in playlists" :key="playlist">
+            <div>
+                <router-link :to="'/playlist/' + playlist.id">
+                    <img :src="playlist.picture_medium" alt="">
+                    <h3>{{playlist.title}}</h3>
+                    <p>Duration: {{playlist.duration}}</p>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+    data() {
+        return {
+            playlists: []
+        }
+    },
+    methods: {
+        fetch_list() {
+            // let url = 'http://localhost:5000/user/get-playlist'
+            // axios.post(url, {
+            //     'token': access_token
+            // }).then((results) => {
+            //     console.log(results)
+            //     this.playlists = results.data.data
+            // }).catch((err) => {
+            //     console.log(err)
+            // })
+            axios({
+                method: 'get',
+                url: 'http://localhost:5000/user/get-playlist',
+                headers: {'Authorization': `Bearer ${localStorage.getItem("deez")}`}
+            }).then((results) => {
+                console.log(results)
+                this.playlists = results.data.data
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+    }
+}
+</script>
