d25c328b578322858ed229f37e3f6ce0a941a491
omputle <omputle@student.wethinkcode.co.za>
2020-10-14 22:28:47 +0200

    created websocket test view

---
+++ client/src/views/WebsocketTest.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <input type="text" name="" id="" v-model="msg">
+        <button @click="send">Send</button>
+
+        <ol>
+            <div v-for="text in texts" :key="text">
+                <li>{{text}}</li>
+            </div>
+        </ol>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: '',
+            connection: null,
+            texts: []
+        }
+    },
+    methods: {
+        send() {
+            this.connection.send(this.msg)
+        }
+    },
+    created() {
+        this.connection = new WebSocket('ws://localhost:5001')
+
+        this.connection.onopen = () => {
+            console.log('Connection opened')
+        }
+
+        this.connection.onmessage = (event) => {
+            this.msg = ''
+            console.log(event.data)
+            this.texts.push(event.data)
+        }
+
+        this.connection.onclose = function() {
+        alert("Closed websocket")
+        }
+    }
+}
+</script>
