ba6cc67d8b1fb426b594f880f60df4ac4089c24e
omputle <omputle@student.wethinkcode.co.za>
2020-10-18 12:40:42 +0200

    added licensed routes

--- client/src/views/Playlist.vue
+++ client/src/views/Playlist.vue
@@ -5,6 +5,14 @@
         <p>{{creator}}</p>
         <router-link :to="'/playlist/' + playlist_id + '/add'">Add songs to {{playlist_name}}</router-link> | 
         <router-link :to="'/playlist/' + playlist_id + '/remove'">Remove songs from {{playlist_name}}</router-link>
+        <div>
+            <h3>Followers</h3>
+            <ol>
+                <div v-for="follower in followers" :key="follower">
+                    <li>{{follower.name}} => <button @click="give_license(follower.id)">Give License</button> | <button @click="remove_license(follower.id)">Remove license</button></li>
+                </div>
+            </ol>
+        </div>
         <div class="songs">
             <ol>
                 <div v-for="song in songs" :key="song">
@@ -31,7 +39,8 @@ export default {
             cover: '',
             playlist_name: '',
             play: '',
-            music: ''
+            music: '',
+            followers:[]
         }
     },
     methods: {
@@ -63,6 +72,57 @@ export default {
                 this.$router.push('/okay')
             })
         },
+        fetch_profile() {
+            axios({
+                method: 'get',
+                url: `http://localhost:5000/user/me`,
+                headers: {'Authorization': `Bearer ${localStorage.getItem("deez")}`}
+            }).then(res => {console.log(res)})
+            .catch(e => {console.log(e)})
+        },
+        fetch_friends() {
+            axios({
+                method: 'get',
+                url: `http://localhost:5000/user/friends`,
+                headers: {'Authorization': `Bearer ${localStorage.getItem("deez")}`}
+            }).then(res => {
+                console.log(res)
+                this.followers = res.data.followers
+            })
+            .catch(e => {console.log(e)})
+        },
+        give_license(user_id) {
+            axios({
+                method: 'post',
+                url: `http://localhost:5000/user/give-license`,
+                data: {
+                    'user_id': user_id,
+                    'playlist_id': this.playlist_id,
+                    'playlist_name': this.playlist_name,
+                    'token': localStorage.getItem("jwt")
+                },
+                headers: {'Authorization': `Bearer ${localStorage.getItem("deez")}`}
+            }).then((res) => {
+                console.log(res)
+            }).catch((err) => {
+                console.log(err)
+            })
+        },
+        remove_license(user_id) {
+            axios({
+                method: 'post',
+                url: `http://localhost:5000/user/revoke-license`,
+                data: {
+                    'user_id': user_id,
+                    'playlist_id': this.playlist_id,
+                },
+                headers: {'Authorization': `Bearer ${localStorage.getItem("deez")}`}
+            }).then((res) => {
+                console.log(res)
+            }).catch((err) => {
+                console.log(err)
+            })
+        },
         play_track(song) {
             this.play = song.preview
             
@@ -70,6 +130,8 @@ export default {
     },
     created() {
         this.fetch_data()
+        this.fetch_profile()
+        this.fetch_friends()
         this.music = `https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=1000&color=EF5466&layout=dark&size=medium&type=playlist&id=${this.playlist_id}&app_id= 437882"`
     }
 }
@@ -79,7 +141,7 @@ export default {
 li {
     text-align: left;
 }
-li:hover {
+.songs li:hover {
     color: brown;
 }
 .songs {
