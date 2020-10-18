bd3ae6a4338d0e2da8c22a9bc61068fecdaed29b
omputle <omputle@student.wethinkcode.co.za>
2020-10-18 12:38:27 +0200

    added new views

---
+++ client/src/views/LicensedPlaylists.vue
@@ -0,0 +1,148 @@
+<template>
+    <div>
+        <h2>{{playlist_name}}</h2>
+        <img :src="cover" alt=""> <br>
+        <p>{{creator}}</p>
+        <router-link :to="'/playlist-license/' + playlist_id + '/add'">Add songs to {{playlist_name}}</router-link> | 
+        <router-link :to="'/playlist-license/' + playlist_id + '/remove'">Remove songs from {{playlist_name}}</router-link>
+        <div>
+        </div>
+        <div class="songs">
+            <ol>
+                <div v-for="song in songs" :key="song">
+                    <li @click="play_track(song)">{{song.title}} - {{song.artist.name}}</li>
+                </div>
+            </ol>
+        </div>
+        <div>
+            <iframe scrolling="yes" frameborder="0" allowTransparency="true" :src="music"></iframe>
+            <!-- <audio :src="play" controls v-if="play" autoplay></audio> -->
+        </div>
+        <!-- <button @click="delete_playlist">Delete playlist {{playlist_name}}</button> -->
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            playlist_id: this.$route.params.id,
+            songs: [],
+            creator: '',
+            cover: '',
+            playlist_name: '',
+            play: '',
+            music: '',
+            followers:[]
+        }
+    },
+    methods: {
+        fetch_data() {
+            axios({
+                method: 'get',
+                url: 'http://localhost:5000/music/playlist-info/' + this.playlist_id,
+                headers: {'Authorization': `Bearer ${localStorage.getItem("deez")}`}
+            }).then((results) => {
+                console.log(results)
+                this.playlist_name = results.data.title
+                this.creator = results.data.creator.name
+                this.cover = results.data.picture_big
+                this.songs = results.data.tracks.data
+            }).catch((err) => {
+                console.log(err)
+            })
+        },
+        delete_playlist() {
+            axios({
+                method: 'delete',
+                url: 'http://localhost:5000/music/delete-playlist',
+                headers: {'Authorization': `Bearer ${localStorage.getItem("deez")}`},
+                data: {
+                    'playlist_id': this.playlist_id
+                }
+            }).then((results) => {
+                console.log(results)
+                this.$router.push('/okay')
+            })
+        },
+        fetch_profile() {
+            axios({
+                method: 'get',
+                url: `http://localhost:5000/user/me`,
+                headers: {'Authorization': `Bearer ${localStorage.getItem("deez")}`}
+            }).then(res => {console.log(res)})
+            .catch(e => {console.log(e)})
+        },
+        fetch_friends() {
+            axios({
+                method: 'get',
+                url: `http://localhost:5000/user/friends`,
+                headers: {'Authorization': `Bearer ${localStorage.getItem("deez")}`}
+            }).then(res => {
+                console.log(res)
+                this.followers = res.data.followers
+            })
+            .catch(e => {console.log(e)})
+        },
+        give_license(user_id) {
+            axios({
+                method: 'post',
+                url: `http://localhost:5000/user/okay`,
+                data: {
+                    'user_id': user_id,
+                    'playlist_id': this.playlist_id,
+                    'playlist_name': this.playlist_name,
+                    'token': localStorage.getItem("jwt")
+                },
+                headers: {'Authorization': `Bearer ${localStorage.getItem("deez")}`}
+            }).then((res) => {
+                console.log(res)
+            }).catch((err) => {
+                console.log(err)
+            })
+        },
+        remove_license(user_id) {
+            axios({
+                method: 'post',
+                url: `http://localhost:5000/user/okay-too`,
+                data: {
+                    'user_id': user_id,
+                    'playlist_id': this.playlist_id,
+                },
+                headers: {'Authorization': `Bearer ${localStorage.getItem("deez")}`}
+            }).then((res) => {
+                console.log(res)
+            }).catch((err) => {
+                console.log(err)
+            })
+        },
+        play_track(song) {
+            this.play = song.preview
+            
+        }
+    },
+    created() {
+        this.fetch_data()
+        this.fetch_profile()
+        this.fetch_friends()
+        this.music = `https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=1000&color=EF5466&layout=dark&size=medium&type=playlist&id=${this.playlist_id}&app_id= 437882"`
+    }
+}
+</script>
+
+<style scoped>
+li {
+    text-align: left;
+}
+.songs li:hover {
+    color: brown;
+}
+.songs {
+    float: right;
+}
+iframe {
+    width: 100%;
+    height: 500px;
+}
+</style>
